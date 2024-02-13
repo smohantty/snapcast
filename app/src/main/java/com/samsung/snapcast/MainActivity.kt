1c32d055e2e67dca15ad57687647fd8fea2bcbf2
subhransu <smohantty@gmail.com>
2024-02-13 22:24:07 +0900

    refactor

--- app/src/main/java/com/samsung/snapcast/MainActivity.kt
+++ app/src/main/java/com/samsung/snapcast/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.samsung.snapcast.ui.AppHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,15 +17,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.samsung.snapcast.designsystem.theme.WallpaperTheme {
+            //com.samsung.snapcast.designsystem.theme.WallpaperTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppHost()
+                    Text(text = "Hello")
                 }
-            }
+            //}
         }
     }
 }
@@ -34,7 +34,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    com.samsung.snapcast.designsystem.theme.WallpaperTheme {
-        AppHost()
-    }
+        Text(text = "Hello")
 }
---
+++ core/designsystem/src/main/java/com/samsung/snapcast/designsystem/icon/HodlIcons.kt
@@ -0,0 +1,24 @@
+package com.samsung.snapcast.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CandlestickChart
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.rounded.CandlestickChart
+import androidx.compose.material.icons.rounded.Face
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+/**
+ * Now in Android icons. Material icons are [ImageVector]s, custom icons are drawable resource IDs.
+ */
+object HodlIcons {
+    val Home = Icons.Rounded.Home
+    val HomeBorder = Icons.Outlined.Home
+    val Bots = Icons.Rounded.CandlestickChart
+    val BotsBorder = Icons.Outlined.CandlestickChart
+    val Settings = Icons.Rounded.Settings
+    val SettingsBorder = Icons.Outlined.Settings
+}
